d22427cfb744b722a6fa95b7ec8a137f9bd548b8
coderhangb <ninjavinh2006@gmail.com>
2026-03-18 21:18:15 +0700

    add signup page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,47 @@
-import { Routes, Route } from "react-router";
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router";
+import { Toaster } from "react-hot-toast";
+import PageLoader from "./components/PageLoader.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import Decors from "./components/Decors.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
-      {/* DECORATORS - GRID BG & GLOW SHAPES */}
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
 
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth)
+    return (
+      <div className="min-h-screen text-slate-100 bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+        <Decors />
+        <PageLoader />
+      </div>
+    );
+
+  return (
+    <div className="min-h-screen text-slate-100 bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      <Decors />
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,40 @@
 import { create } from "zustand";
+import { axiosInstance } from "../libs/aixos.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-  authUser: { name: "John" },
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/api/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/api/auth/signup", data);
+      set({ authUser: res.data });
+
+      // toast from react hot toast
+      toast.success("Account created successfully!");
+    } catch (error) {
+      toast.error(
+        error.response.data.fullName ||
+          error.response.data.email ||
+          error.response.data.password,
+      );
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
 }));
