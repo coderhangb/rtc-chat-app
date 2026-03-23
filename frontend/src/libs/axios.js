561e272ddf3d2bacef3334a62c43e220a588ef8a
coderhangb <ninjavinh2006@gmail.com>
2026-03-23 21:38:38 +0700

    fix backend endpoint

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://rtc-chat-app-1ql9.onrender.com",
   withCredentials: true,
 });
